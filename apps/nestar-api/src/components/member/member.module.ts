7675779cf635a9e5b1c2adeb3921b33660841453
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-08 22:35:17 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,7 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, UsePipes, ValidationPipe } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
-import { Mutation, Query } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 
@@ -11,30 +10,4 @@ import MemberSchema from '../../schemas/Member.model';
   ],
   providers: [MemberResolver, MemberService]
 })
-export class MemberModule {
-  constructor(private readonly memberService: MemberService){}
-
-  @Mutation(() => String)
-  public async signup(): Promise<string>{
-    console.log("Mutation: signup");
-    return this.memberService.signup();
-  }
-
-  @Mutation(() => String)
-  public async login(): Promise<string>{
-    console.log("Mutation: login");
-    return this.memberService.login();
-  }
-  
-  @Mutation(() => String)
-  public async updateMember(): Promise<string>{
-    console.log("Mutation: updateMember");
-    return this.memberService.updateMember();
-  }
-
-  @Query(() => String)
-  public async getMember(): Promise<string>{
-    console.log("Query: getMember");
-    return this.memberService.getMember();
-  }
-}
+export class MemberModule {}
