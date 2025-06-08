fdc55f3f58e6a41887ec0af59eed5aa70905638c
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-08 20:28:23 +0900

    feat: create components related modules

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver } from "@nestjs/apollo"
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(), 
@@ -16,7 +17,8 @@ import { DatabaseModule } from './database/database.module';
       uploads: false,
       autoSchemaFile: true
     }), 
-    ComponentsModule, DatabaseModule
+    ComponentsModule,
+    DatabaseModule,
 ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
