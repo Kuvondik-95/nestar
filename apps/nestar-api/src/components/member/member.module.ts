d0bae2c2f36e089319245a7aaf63ce66e57e0b68
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-08 21:24:30 +0900

    feat: create member resolver & service model

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,4 +1,35 @@
 import { Module } from '@nestjs/common';
+import { MemberResolver } from './member.resolver';
+import { MemberService } from './member.service';
+import { Mutation } from '@nestjs/graphql';
 
-@Module({})
-export class MemberModule {}
+@Module({
+  providers: [MemberResolver, MemberService]
+})
+export class MemberModule {
+  constructor(private readonly memberService: MemberService){}
+
+  @Mutation(() => String)
+  public async signup(): Promise<string>{
+    console.log("Mutation: signup");
+    return this.memberService.signup();
+  }
+
+  @Mutation(() => String)
+  public async login(): Promise<string>{
+    console.log("Mutation: login");
+    return this.memberService.login();
+  }
+  
+  @Mutation(() => String)
+  public async updateMember(): Promise<string>{
+    console.log("Mutation: updateMember");
+    return this.memberService.updateMember();
+  }
+
+  @Mutation(() => String)
+  public async getMember(): Promise<string>{
+    console.log("Mutation: getMember");
+    return this.memberService.getMember();
+  }
+}
