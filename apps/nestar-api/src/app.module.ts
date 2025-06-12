3e0c6d2dcc8a199c6dcca0134a328791e04fe009
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-12 20:17:21 +0900

    fix: modify nest cli detail

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -10,12 +10,12 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), 
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      playground: true,
-      uploads: false,
-      autoSchemaFile: true
+      ConfigModule.forRoot(), 
+      GraphQLModule.forRoot({
+        driver: ApolloDriver,
+        playground: true,
+        uploads: false,
+        autoSchemaFile: true
     }), 
     ComponentsModule,
     DatabaseModule,
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -28,7 +28,7 @@ export class MemberService {
     const response: Member = await this.memberModel
     .findOne({ memberNick: memberNick })
     .select('+memberPassword')
-    .exec() as unknown as Member;
+    .exec();
 
     if(!response || response.memberStatus === MemberStatus.DELETE) {
       throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
