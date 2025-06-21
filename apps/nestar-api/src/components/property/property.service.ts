7196c0938a2ce158abe2fd8052250ae279f5cd5a
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-21 23:22:32 +0900

    feat: develop removePropertyByAdmin GraphQL API

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -11,7 +11,7 @@ import { StatisticModifier, T } from '../../libs/types/common';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
-import moment from 'moment';
+import * as moment from 'moment';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 
 @Injectable()
@@ -255,6 +255,14 @@ export class PropertyService {
       });
     }
 
+    return result;
+  }
+
+  public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property>{
+    const search: T = {_id: propertyId, propertyStatus: PropertyStatus.DELETE}
+    const result = await this.propertyModel.findOneAndDelete(search).exec();
+    if(!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+    
     return result;
   }
 }
