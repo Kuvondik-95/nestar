4db767f76e69ee104516c54e8ac7563a743a0e8c
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-24 17:06:07 +0900

    feat: develop createComment, updateComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -14,6 +14,7 @@ import { MemberModule } from '../member/member.module';
       ViewModule,
       MemberModule
     ],
-  providers: [PropertyService, PropertyResolver]
+  providers: [PropertyService, PropertyResolver],
+  exports: [PropertyService]
 })
 export class PropertyModule {}
