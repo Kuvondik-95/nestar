dfa5eb686fa6f94c42ebb2693655e41dbe0c76d6
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-27 17:13:04 +0900

    feat: develop likeTargetBoardArticle GraphQL API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -11,6 +11,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { LikeService } from '../like/like.service';
 
 @Injectable()
 export class BoardArticleService {
@@ -19,6 +22,7 @@ export class BoardArticleService {
       // private authService: AuthService,
       private viewService: ViewService,
       private memberService: MemberService,
+      private likeService: LikeService,
     ) {}
 
   public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle>{
@@ -118,6 +122,37 @@ export class BoardArticleService {
     return result[0];
   }
 
+  public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle>{
+      const target: BoardArticle = await this.boardArticleModel
+      .findOne(
+        { _id: likeRefId, 
+          articleStatus: BoardArticleStatus.ACTIVE
+        })
+      .exec()
+      if(!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+  
+      const input: LikeInput = {
+        memberId: memberId,
+        likeRefId: likeRefId,
+        likeGroup: LikeGroup.ARTICLE
+      };
+  
+      // LIKE TOGGLE
+      const modifier: number = await this.likeService.toggleLike(input);
+      const result = await this.boardArticleStatsEditor(
+        {
+          _id: likeRefId,
+          targetKey: 'articleLikes',
+          modifier: modifier,
+        }
+      );
+      if(!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+  
+      return result;
+    }
+
+
+  /** ADMIN **/
   public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles>{
     const { articleStatus, articleCategory} = input.search;
     const match: T = {};
